79be88ec347c71e978e9cb91d9b4e7ba6f266a3b
BestDingSheng <727040141@qq.com>
2018-01-18 15:51:03 +0800

    添加新功能

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 dingsheng
 ---
-test demo
 
-## Install
+基于nodejs开发的自己的cli工具 
+
+
+### 功能
+
+-  翻译单词
+-  快速新建vue和react项目
+-  开发中ing
+
+## 安装
 
 ```
 [sudo] npm install dingsheng  [-g]
 
 ```
 
-## Usage
+## 使用
 
+翻译
 ```
 $ ding-cli chrome
 
@@ -19,6 +28,16 @@ $ ding-cli chrome
  美音: [krom]   英音: [krəʊm]
 
  翻译：n. 铬，铬合金；铬黄；谷歌浏览器
+
+```
+
+创建vue或react项目
+
+```
+➜  dingsheng-cli git:(master) ✗ ding-cli init --vue
+正在拉取模板代码
+下载位置：/Users/deson/DesonProject/opensource/dingsheng-cli/vueproject/ ...
+模板工程建立完成
 
 ```
 
--- bin/index.js
+++ bin/index.js
@@ -6,32 +6,54 @@ var {
 var program = require('commander'),
     chalk = require('chalk');
 
+var shell = require('shelljs')
+var clone = require('git-clone')
 
 program
-    .version('1.0.0')
-    .description('基于有道api的翻译工具')
+    .version(require('../package').version)
+    .description('一个可以翻译和快速创建vue和react项目的工具')
+
 program
     .command('init')
-    .description('功能待开发')
-    .action(function () {
-      console.log('功能待开发')
+    .option('--vue', '初始化vue项目')
+    .option('--react', '初始化react项目')
+    .description('初始化前端项目')
+    .action(function(option) {
+        let vue = option.vue;
+        let react = option.react;
+        if (vue) {
+            downProject('https://github.com/BestDingSheng/ding-initVue.git',"vueproject");
+        } else if (react) {
+             downProject('https://github.com/BestDingSheng/react-redux-demo.git',"reactproject");
+        } else {
+            console.log(chalk.cyan('请输入正确的参数'));
+            console.log(chalk.cyan('ding-cli init --vue'));
+            console.log(chalk.cyan('ding-cli init --react'));
+        }
     })
 program
     .command('* <text>')
     .description('比如ding-cli node 会翻译node')
-    .action(function (text) {
+    .action(function(text) {
         if (text) {
             api(text)
         } else {
             console.log(chalk.cyan('请填写需要翻译的单词'));
         }
-
     })
-
 program.parse(process.argv)
-
-
 let query = process.argv[2];
-if(!query){
+if (!query) {
     program.help();
+} 
+// 下载模板
+function downProject(url,filename) {
+    let pwd = shell.pwd()
+    let project = filename
+    console.log(chalk.blue('正在拉取模板代码'));
+    console.log(`下载位置：${pwd}/${project}/ ...`)
+    clone(`${url}`, pwd + `/${project}`, null, function() {
+        shell.rm('-rf', pwd + `/${project}/.git`)
+        console.log('模板工程建立完成')
+    })
 }
